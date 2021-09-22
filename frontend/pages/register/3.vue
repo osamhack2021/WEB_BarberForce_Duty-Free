3e8f7f09ce85c9d48042dec58cf490bda18a43d9
Chanhyuk Byeon <bdu00chch@naver.com>
2021-09-22 14:01:19 +0000

    Register page prototype (just paing, basic style)

---
+++ frontend/pages/register/3.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="p-2">
+    <form @submit.prevent>
+      <div class="mb-6">
+        <label class="block mb-1" for="email">이메일을 입력해주세요.</label>
+        <input
+          id="email"
+          class="w-full border rounded p-2 text-sm focus:outline-none focus:border-blue-500 focus:shadow-sm"
+          type="text"
+          name="email"
+        />
+      </div>
+      <div class="mb-6">
+        <label class="block mb-1" for="password">비밀번호를 입력해주세요.</label>
+        <input
+          id="password"
+          class="w-full border rounded p-2 text-sm focus:outline-none focus:border-blue-500 focus:shadow-sm"
+          type="password"
+          name="password"
+        />
+      </div>
+      <div class="mb-6">
+        <label class="block mb-1" for="password_confirm">비밀번호를 다시 입력해주세요.</label>
+        <input
+          id="password_confirm"
+          class="w-full border rounded p-2 text-sm focus:outline-none focus:border-blue-500 focus:shadow-sm"
+          type="password"
+          name="password_confirm"
+        />
+      </div>
+      <div class="flex justify-end">
+        <NuxtLink class="rounded bg-blue-500 text-white py-2 px-3" to="/register/4">다음</NuxtLink>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  transition: 'slide',
+};
+</script>
+
+<style></style>
---
+++ frontend/pages/register/4.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="p-2">
+    <form @submit.prevent>
+      <div class="mb-6">
+        <label class="block mb-1" for="email">이름을 알려주세요.</label>
+        <input
+          id="name"
+          class="w-full border rounded p-2 text-sm focus:outline-none focus:border-blue-500 focus:shadow-sm"
+          type="text"
+          name="name"
+        />
+      </div>
+      <div class="mb-6">
+        <label class="block mb-1" for="password">군번을 입력해주세요.</label>
+        <input
+          id="soldier_id"
+          class="w-full border rounded p-2 text-sm focus:outline-none focus:border-blue-500 focus:shadow-sm"
+          type="text"
+          name="soldier_id"
+        />
+      </div>
+      <div class="mb-6">
+        <div class="mb-4">휴대전화로 본인인증을 해주세요.</div>
+        <div class="flex justify-center">
+          <img class="w-16 h-16" src="~/assets/img/pass.png" alt="PASS" />
+        </div>
+      </div>
+      <div class="flex justify-center">
+        <button class="rounded bg-blue-500 text-white py-2 px-3">가입하기</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  transition: 'slide',
+};
+</script>
+
+<style></style>
