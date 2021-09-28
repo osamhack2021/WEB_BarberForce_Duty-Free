a92ed789f64d50fa9ed60a2e961210dd627b2184
sw0501 <dkxkqkrktddn@naver.com>
2021-09-28 22:37:01 +0900

    modify server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const path = require('path');
+//const bodyParser = require('body-parser');
+//const ejs = require('ejs');
+//const path = require('path');
 
 
 app.get('/', (req, res) => {
