c2a30c6ed7503504bfdab43bfc2a61f0241df84e
sw0501 <dkxkqkrktddn@naver.com>
2021-09-28 22:12:17 +0900

    organize file

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const path = require('path');
+
+
+app.get('/', (req, res) => {
+    res.json({
+        success: true,
+    });
+});
+
+app.get('/login',(req,res) =>{
+
+})
+
+app.get('/register',(req,res) =>{
+
+})
+
+app.get('/me',(req,res) =>{
+
+})
+
+app.listen(port, () => {
+    console.log(`server is listening at localhost:${process.env.PORT}`);
+});
