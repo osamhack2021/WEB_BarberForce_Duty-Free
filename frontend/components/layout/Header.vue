584cde3de6cc66195f7cf9110f5cb4ece838e9e1
sw0501 <dkxkqkrktddn@naver.com>
2021-10-01 00:09:23 +0900

    reset

---
+++ frontend/components/layout/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="border shadow">
+    <div class="container flex mx-auto p-3">
+      <NuxtLink to="/">
+        <img class="w-12 h-12" src="@/assets/img/logo.png" alt="Barber Force" />
+      </NuxtLink>
+      <button class="ml-auto focus:outline-none">
+        <img class="w-6 h-6" src="@/assets/img/menu.svg" />
+      </button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
