ee29f2c61388b90b1581c5f948f9c34937e650d5
Chanhyuk Byeon <bdu00chch@naver.com>
2021-10-02 23:52:43 +0900

    Main page styling (for customer) & Fluid header (#10)

---
+++ frontend/components/common/Heading.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex justify-center">
+    <span class="tracking-wider font-bold border-b-2 border-brand px-2 pb-1">
+      <slot></slot>
+    </span>
+  </div>
+</template>
+
+<style></style>
--- frontend/components/layout/Header.vue
+++ frontend/components/layout/Header.vue
@@ -1,11 +1,12 @@
 <template>
   <header class="shadow bg-brand">
-    <div class="relative container flex mx-auto h-full px-4">
+    <div class="relative flex h-full px-4">
       <button class="focus:outline-none">
         <img src="@/assets/img/menu.svg" style="width: 23px" />
       </button>
       <NuxtLink class="absolute top-1/2 left-1/2" style="transform: translate(-50%, -50%)" to="/">
-        <img src="@/assets/img/logo_white.png" alt="Barber Force" />
+        <img class="block md:hidden" src="@/assets/img/logo_white.png" alt="Barber Force" />
+        <img class="hidden md:block" src="@/assets/img/logo_with_title.png" alt="Barber Force" />
       </NuxtLink>
     </div>
   </header>
@@ -15,4 +16,8 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+header {
+  z-index: 99999;
+}
+</style>
