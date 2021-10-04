a11222df3305f2d30a9dec0911e3ca9fe9a76919
sw0501 <41150708+sw0501@users.noreply.github.com>
2021-10-05 03:26:23 +0900

    master brach pull & add login session

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,91 @@
 const express = require('express');
 const app = express();
 const port = 3306;
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const key = require('./auth/key');
 //const ejs = require('ejs');
-//const path = require('path');
-//const db = require('./db.js');
+const path = require('path');
+const db = require('./db');
+const User = require('./user');
+
 //const route = require('./route.js');
 
 //app.set('view engine','pug');
 //app.set('views',path.join(__dirname,'Form.html'));
-//db();
+db();
 //app.use(express.static(path.join(__dirname,'Form.html')));
 //app.use('/',route);
 
+app.use(bodyParser.json());
+/*
+app.get('/',function(req,res){
+  console.log(req.body);
+    res.send(req.body);
+});
 
 app.get('/', (req, res) => {
     res.json({
         success: true,
     });
 });
+*/
 
+app.get('/',(req,res)=>{
+  User.find({})
+  .then( users=>{
+    res.send(users);
+  });
+})
+
+
+app.post('/login',(req,res)=>{
+  User.findOne({email: req.body.email}, (err,user)=>{
+    if(!user){
+      return res.json({
+        mess: req.body.email,
+        loginSuccess: false,
+        message: "Unvalid email"
+      });
+    }
+    user.comparePassword(req.body.password, (err, isMatch)=>{
+      if(!isMatch)
+        return res.json({
+          loginSuccess:false,
+          message: "Wrong password"
+        });
+        user.generateToken((err, user)=>{
+                if(err) return res.status(400).send(err);
+                // 토큰을 쿠키에 저장
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({
+                    loginSuccess: true,
+                    userId: user._id
+            });
+        });
+    });
+  });
+});
+
+app.get('/login',(req,res)=>{
+  res.send(req.body);
+})
+
+app.post('/check_login',(req,res)=>{
+  const token = req.body['jwt'];
+
+  jwt.verify(token,key,(err,decoded)=>{
+    if(!err && decoded.username == 'root')
+    {
+      res.send('로그인이 되어있으시네요!');
+    }
+    else{
+      res.send('저런, 로그인이 되어있지 않네요.');
+    }
+  });
+//
+});
 /*
 app.get('/login',(req,res) =>{
 
--- backend/user.js
+++ backend/user.js
@@ -1,6 +1,30 @@
 const mongoose = require('mongoose');
+const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
+
 const userSchema = new mongoose.Schema({
-  name: String,
-  data: Object,
+  email: String,
+  password: String
 });
+
+userSchema.methods.comparePassword=function(plainPassword, cb){
+  if(plainPassword==this.password)return cb(null,true);
+  /*
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch);
+    });
+  */
+}
+
+userSchema.methods.generateToken=function(cb){
+    const user=this;
+    const token=jwt.sign(user._id.toHexString(), 'secretToken');
+    user.token=token;
+    user.save(function(err, user){
+        if(err) return cb(err);
+        cb(null, user);
+    });
+}
+
 module.exports = mongoose.model('User', userSchema);
