d7f7c3dff9df289a13ae3c47418d36f44c5c960a
Bisue <bdu00chch@naver.com>
2021-10-09 05:39:47 +0900

    fix: html lang 수정 (모바일에서 번역뜨는거 방지)

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
+    htmlAttrs: {
+      lang: 'ko',
+    },
     title: 'barber-force-frontend',
     meta: [
       { charset: 'utf-8' },
