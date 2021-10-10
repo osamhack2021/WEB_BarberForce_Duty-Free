808bf7d2c4f48b1e85cdde1ac63cfb87b2577032
Bisue <bdu00chch@naver.com>
2021-10-10 13:59:05 +0900

    manifest 설정 (feat. @nuxtjs/pwa)

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -55,7 +55,8 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en',
+      name: 'Barber Force',
+      lang: 'ko',
     },
   },
 
