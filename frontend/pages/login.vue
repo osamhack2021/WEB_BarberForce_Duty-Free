bd11363057eb5a73d85ed86852602702e418c42e
Chanhyuk Byeon <bdu00chch@naver.com>
2021-10-12 00:41:30 +0900

    Frontend 잡다한 리팩토링 (1) (#23)
    * index 상단 Carousel 컴포넌트화
    
    * plugin/auth.js -> plugin/fetch-user.js
    
    * auth, guest middleware toast 메세지 추가
    
    * auth 모듈 추가 (추후 소셜로그인도 추가예정)
    
    * register 후 폼 state 비우기
    
    * 중복컬러 통합 (bg-auth => bg-brand)
    
    * 불필요한 더미 vuex module 삭제
    
    * Header height 매직넘버 대체

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="h-screen w-screen flex flex-col items-center justify-center bg-auth p-3">
+  <main class="h-screen w-screen flex flex-col items-center justify-center bg-brand p-3">
     <!-- logo -->
     <img src="@/assets/img/1_logo+title_figma.svg" width="203" alt="BarberForce" />
     <!-- form -->
@@ -51,20 +51,22 @@ export default {
   methods: {
     async login() {
       try {
-        await this.$store.dispatch('auth/login', this.credentials);
-        this.$toast.success('성공적으로 로그인되었습니다!');
-        this.$router.replace('/');
+        await this.$auth.login(this.credentials);
       } catch (e) {
-        this.$toast.error('아이디와 비밀번호를 확인해주세요!');
+        console.error(e);
+        if (e.response && e.response.data && e.response.data.message) {
+          const message = e.response.data.message;
+          if (message === 'Unvalid email') {
+            this.$toast.error('해당 이메일과 일치하는 계정이 없습니다!');
+          } else if (message === 'Wrong password') {
+            this.$toast.error('비밀번호가 맞지 않습니다!');
+          }
+        } else {
+          this.$toast.error('알 수 없는 에러가 발생했습니다!');
+        }
         this.credentials.password = '';
       }
     },
   },
 };
 </script>
-
-<style scoped>
-.bg-auth {
-  background-color: #406d96;
-}
-</style>
