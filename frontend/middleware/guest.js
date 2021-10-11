405c26c363d3e52076f1884cace8a35848de3924
Bisue <bdu00chch@naver.com>
2021-10-11 21:25:20 +0900

    auth, guest middleware toast 메세지 추가

--- frontend/middleware/guest.js
+++ frontend/middleware/guest.js
@@ -1,5 +1,6 @@
-export default function ({ store, redirect }) {
+export default function ({ $toast, store, redirect }) {
   if (store.getters['auth/isLoggedIn']) {
+    $toast.error('로그인하지 않은 상태에서만 접근 가능합니다!');
     return redirect('/');
   }
 }
