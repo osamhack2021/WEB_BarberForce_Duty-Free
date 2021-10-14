3a7c686dfc7282aac576f0bf7cc1ff85c87f0d8f
Bisue <bdu00chch@naver.com>
2021-10-15 07:39:55 +0900

    /setDummyData 라우트 (미용실, 부대(unit) 더미데이터 추가용)

--- WEB(BE)/server.js
+++ WEB(BE)/server.js
@@ -87,8 +87,141 @@ app.post('/createBarbers', (req, res) => {
   });
 });
 
-app.post('/createUnit', (req, res) => {
-  Unit.insertMany({ unitName: req.body.unitName, soldier_id: req.body.soldier_id, barbers_id: req.body.barbers_id });
+app.get('/setDummyData', async (req, res) => {
+  // 기존 barbers, unit 데이터 비우기
+  await Barbers.deleteMany({});
+  await Unit.deleteMany({});
+
+  // 부대 데이터
+  const units = [
+    {
+      unitName: 'OICW',
+      soldier_id: [],
+      barbers_id: [],
+    },
+    {
+      unitName: 'AFOC',
+      soldier_id: [],
+      barbers_id: [],
+    },
+    {
+      unitName: 'AAOC',
+      soldier_id: [],
+      barbers_id: [],
+    },
+  ];
+  /**
+   * 군번 목록
+   * 20-70007000 ~ 20-70007299 : OICW
+   * 20-70007300 ~ 20-70007699 : AFOC
+   * 20-70007700 ~ 20-70007999 : AAOC
+   */
+  for (let i = 0; i < 999; i++) {
+    const dummySoliderId = `20-${70007000 + i}`;
+    if (i < 300) {
+      units[0].soldier_id.push(dummySoliderId);
+    } else if (i < 700) {
+      units[1].soldier_id.push(dummySoliderId);
+    } else {
+      units[2].soldier_id.push(dummySoliderId);
+    }
+  }
+  for (const dummy of units) {
+    await Unit.create(dummy);
+  }
+
+  // 미용실
+  const barbers = [
+    {
+      title: `Yang's BarberShop`,
+      location: '보라매 사동',
+      location_detail: '경기도 평택시 고덕북로 77 불난버섯집 1층 중앙현관',
+      description: '매주 월요일 휴무입니다.',
+      rating: 4,
+      phone: '010-0000-0011',
+      thumb: '/img/shop2.jpg',
+      bookmarked: false,
+      partnership: ['OICW', 'AFOC'],
+    },
+    {
+      title: `송탄이발소`,
+      location: '송탄역',
+      location_detail: '경기도 평택시 고덕북로 77 불난버섯집 1층 중앙현관',
+      description: '매주 화요일 휴무입니다.',
+      rating: 4.2,
+      phone: '010-0000-0011',
+      thumb: '/img/shop1.jpg',
+      bookmarked: false,
+      partnership: ['OICW', 'AFOC'],
+    },
+    {
+      title: `기본군사훈련단`,
+      location: '진주',
+      location_detail: '경기도 평택시 고덕북로 77 불난버섯집 1층 중앙현관',
+      description: '연중무휴입니다.',
+      rating: 4,
+      phone: '010-0000-0011',
+      thumb: '/img/shop2.jpg',
+      bookmarked: false,
+      partnership: ['OICW', 'AFOC'],
+    },
+    {
+      title: `제 20 전투비행단`,
+      location: '서산',
+      location_detail: '경기도 평택시 고덕북로 77 불난버섯집 1층 중앙현관',
+      description: '매주 화요일 휴무입니다.',
+      rating: 4.2,
+      phone: '010-0000-0011',
+      thumb: '/img/shop1.jpg',
+      bookmarked: false,
+      partnership: ['OICW', 'AFOC'],
+    },
+    {
+      title: `머리 잘하는 집`,
+      location: '송탄출장소',
+      location_detail: '경기도 평택시 고덕북로 77 불난버섯집 1층 중앙현관',
+      description: '매주 수요일 휴무입니다.',
+      rating: 4.5,
+      phone: '010-0010-0052',
+      thumb: '/img/shop1.jpg',
+      bookmarked: false,
+      partnership: ['AAOC'],
+    },
+    {
+      title: `송탄미용실`,
+      location: 'K-55 정문',
+      location_detail: '경기도 평택시 고덕북로 77 불난버섯집 1층 중앙현관',
+      description: '매주 수요일 휴무입니다.',
+      rating: 3.5,
+      phone: '010-0110-1000',
+      thumb: '/img/shop1.jpg',
+      bookmarked: false,
+      partnership: ['AAOC'],
+    },
+  ];
+  for (const dummy of barbers) {
+    const created = await Barbers.create(dummy);
+    // 미용실의 partnership에 대해 모두 등록
+    for (const partner of dummy.partnership) {
+      const unit = await Unit.findOne({ unitName: partner });
+      unit.barbers_id.push(created._id);
+      unit.save();
+    }
+  }
+  res.json('done');
+});
+
+app.get('/deleteDummys', async (req, res) => {
+  res.json('done');
+});
+
+app.post('/createUnit', async (req, res) => {
+  const response = await Unit.create({
+    unitName: req.body.unitName,
+    solider_id: req.body.soldier_id,
+    barbers_id: req.body.barbers_id,
+  });
+  console.log('res', response);
   return res.json({
     mss: '추가',
   });
