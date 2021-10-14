e0a54958d6ecce940af0805b0bbf4a03e28ce29f
Bisue <bdu00chch@naver.com>
2021-10-15 04:57:36 +0900

    fix: fetchUser middleware 동작하도록 수정

--- WEB(BE)/middleware/fetchUser.js
+++ WEB(BE)/middleware/fetchUser.js
@@ -1,10 +1,14 @@
 const jwt = require('jsonwebtoken');
-const userModel = require('../models/user');
+const UserModel = require('../models/user');
 
 // 나중에 진짜 시크릿키로 바꾸기
 // 토큰 만들때 매직스트링으로 했길래 여기도 맞춤.
 const JWT_KEY = 'secretToken';
 
+// 요청의 authorization 헤더로부터 토큰을 검증한 뒤,
+// 토큰을 복호화해 _id 를 찾고 해당하는 사용자를 가져와
+// 라우트에서 바로 쓸 수 있도록 req.user에 할당해주는 미들웨어
+// (이 미들웨어는 로그인을 해야 접근할 수 있게 할 라우트에 적용)
 const fetchUserMiddleware = (req, res, next) => {
   // authorization 헤더가 없는 요청이면 기각
   if (!req.headers.authorization) {
@@ -15,25 +19,37 @@ const fetchUserMiddleware = (req, res, next) => {
 
   // 헤더에서 token 추출
   const token = req.headers.authorization.split(' ')[1];
-  // 토큰 검증
-  jwt.verify(token, JWT_KEY, (e, decoded) => {
-    if (e) {
-      res.status(403).json({
-        message: 'fetchUserMiddleware: invaild token',
+
+  // 토큰 검증 & decode
+  let decoded;
+  try {
+    decoded = jwt.verify(token, JWT_KEY);
+  } catch (e) {
+    // 검증 실패
+    return res.status(403).json({
+      message: 'fetchUserMiddleware: invaild token',
+    });
+  }
+
+  // decode 한 토큰 안의 _id 로 user를 찾음
+  UserModel.findOne({ _id: decoded._id }, (error, user) => {
+    if (error) {
+      return res.status(500).json({
+        message: 'fetchUserMiddleware: SERVER ERROR',
       });
     }
 
-    // 토큰에서 빼온 _id로 해당 유저 찾기 (만들때 넣어줌)
-    // + 이렇게 하면 DB에 토큰을 저장할 필요가 없음!
-    const user = userModel.findOne({ _id: decoded._id });
+    // 해당 _id의 유저가 없으면
     if (!user) {
-      res.status(403).json({
-        message: 'fetchUserMiddleware: invaild token',
+      return res.status(403).json({
+        message: 'fetchUserMiddleware: User not found',
       });
     }
+
+    // req.user 에 user 변수 등록
     req.user = user;
 
-    // 다음 라우트로 이동 (미들웨어로 쓸거기 때문에)
+    // 다음 핸들러로 이동 (미들웨어로 쓸거기 때문에)
     next();
   });
 };
