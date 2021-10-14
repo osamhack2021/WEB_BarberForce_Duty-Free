b28e1938698371c9f4125028c589f155538c90cc
Bisue <bdu00chch@naver.com>
2021-10-15 00:48:22 +0900

    Kakao relative pages (for backend test)

---
+++ WEB(FE)/pages/kakao/callback.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    당신의 토큰: {{ token }}<br />
+    입니다 하하하하하하하<br />
+    <br />
+    여기는 두번째 이후 카카오 로그인부터 오는 페이지
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    token() {
+      return this.$route.query.token;
+    },
+  },
+};
+</script>
+
+<style></style>
