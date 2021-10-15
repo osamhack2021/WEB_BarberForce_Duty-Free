8b5e7cbeb7378a871e53d6df29ab1d48a51a08b8
Bisue <bdu00chch@naver.com>
2021-10-15 15:05:05 +0900

    fix: logical error

--- WEB(BE)/routes/reservations.js
+++ WEB(BE)/routes/reservations.js
@@ -89,7 +89,7 @@ router.post('/reservations/:id/done', fetchUser, async (req, res) => {
   const user = req.user;
 
   try {
-    const reservation = await Reservation.findOne({ user: user._id });
+    const reservation = await Reservation.findOne({ barber: req.params.id });
 
     await reservation.update({ $set: { done: true } });
 
