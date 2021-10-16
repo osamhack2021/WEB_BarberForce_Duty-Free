5db45d24b41825e723d69f25522e570bac7b2b8a
Chanhyuk Byeon <bdu00chch@naver.com>
2021-10-17 00:31:26 +0900

    frontend-improve (#68)
    * MenuBar max-width
    
    * Menubar overlay on pc opened
    
    * 메인페이지 auth 메세지 예외처리
    
    * 메인 '미용실 정보' to /barbers
    
    * fix: overlay 남아있는거
    
    * 예약 정정/취소 구현
    
    * fix: syntax erro

--- WEB(FE)/middleware/auth.js
+++ WEB(FE)/middleware/auth.js
@@ -1,6 +1,6 @@
-export default function ({ $toast, store, redirect }) {
+export default function ({ $toast, route, store, redirect }) {
   if (!store.getters['auth/isLoggedIn']) {
-    $toast.error('로그인한 상태에서만 접근 가능합니다!');
+    if (route.path !== '/') $toast.error('로그인한 상태에서만 접근 가능합니다!');
     return redirect('/auth');
   }
 }
