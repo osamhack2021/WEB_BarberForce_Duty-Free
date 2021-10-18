b4e654e9d3f01aea7e8f9173b6db5892b1c1ee66
Bisue <bdu00chch@naver.com>
2021-10-19 00:57:57 +0900

    복수형-단수형

--- WEB(BE)/routes/boards.js
+++ WEB(BE)/routes/boards.js
@@ -11,10 +11,10 @@ router.get('/boards', fetchUser, async (req, res) => {
   try {
     const board = req.query.board;
 
-    const post = await Board.find({board: board}).populate('user').populate('comment');
+    const post = await Board.find({ board: board }).populate('user').populate('comment');
 
     return res.json({
-      posts: post
+      posts: post,
     });
   } catch (e) {
     console.error(`[${req.method}] ${req.path} - 에러!`, e);
@@ -30,14 +30,12 @@ router.get('/boards', fetchUser, async (req, res) => {
 router.get('/boards/:id', fetchUser, async (req, res) => {
   const user = req.user;
   try {
-
-    const post = await Board.findOne({_id:req.params.id}).populate('user').populate('comment');
+    const post = await Board.findOne({ _id: req.params.id }).populate('user').populate('comment');
 
     return res.json({
-      posts: post,
-      recommend_flag: post.recommend_user.find(_id => _id === user._id) === undefined
+      post: post,
+      recommend_flag: post.recommend_user.find(_id => _id === user._id) === undefined,
     });
-
   } catch (e) {
     console.error(`[${req.method}] ${req.path} - 에러!`, e);
     return res.status(500).json({
@@ -51,13 +49,12 @@ router.get('/boards/:id', fetchUser, async (req, res) => {
 // (async/await)
 router.post('/boards/register', fetchUser, async (req, res) => {
   try {
-
     await Board.create({
       title: req.body.title,
       user: req.user._id,
       body: req.body.body,
-      board: req.query.board
-    })
+      board: req.query.board,
+    });
 
     return res.json({});
   } catch (e) {
@@ -73,12 +70,14 @@ router.post('/boards/register', fetchUser, async (req, res) => {
 // (async/await)
 router.post('/boards/:id/update', fetchUser, async (req, res) => {
   try {
-    const post = await Board.findOne({_id: req.params.id});
-    await post.update({$set: {
-      title: req.body.title,
-      body: req.body.body,
-      board: req.query.board
-    }})
+    const post = await Board.findOne({ _id: req.params.id });
+    await post.update({
+      $set: {
+        title: req.body.title,
+        body: req.body.body,
+        board: req.query.board,
+      },
+    });
     return res.json({});
   } catch (e) {
     console.error(`[${req.method}] ${req.path} - 에러!`, e);
@@ -93,22 +92,15 @@ router.post('/boards/:id/update', fetchUser, async (req, res) => {
 // (async/await)
 router.post('/boards/:id/recommendation', fetchUser, async (req, res) => {
   try {
-    const post = Board.findOne({_id: req.params.id});
+    const post = Board.findOne({ _id: req.params.id });
 
-    const recommend_user = await Board.findOne({_id: req.params.id,recommend_user: req.user._id});
+    const recommend_user = await Board.findOne({ _id: req.params.id, recommend_user: req.user._id });
 
     //추천인 목록에 있는 걍우
-    if(recommend_user){
-      await post.updateMany(
-        {$set: {recommendation: -1,}},
-        {$pull: {recommend_user: req.params.id}}
-      )
-    }
-    else{
-      await post.updateMany(
-        {$set: {recommendation: 1,}},
-        {$push: {recommend_user: req.params.id}}
-      )
+    if (recommend_user) {
+      await post.updateMany({ $set: { recommendation: -1 } }, { $pull: { recommend_user: req.params.id } });
+    } else {
+      await post.updateMany({ $set: { recommendation: 1 } }, { $push: { recommend_user: req.params.id } });
     }
     return res.json({});
   } catch (e) {
@@ -120,15 +112,14 @@ router.post('/boards/:id/recommendation', fetchUser, async (req, res) => {
   }
 });
 
-
 // 게시글 삭제
 // (async/await)
 router.post('/boards/:id/delete', fetchUser, async (req, res) => {
   try {
     //댓글 삭제
-    await Comment.deleteMany({post:req.params.id});
+    await Comment.deleteMany({ post: req.params.id });
     //게시글 삭제
-    await Board.deleteOne({_id:req.params.id});
+    await Board.deleteOne({ _id: req.params.id });
     return res.json({});
   } catch (e) {
     console.error(`[${req.method}] ${req.path} - 에러!`, e);
@@ -147,16 +138,14 @@ router.post('/boards/:id/comments', fetchUser, async (req, res) => {
     await Comment.create({
       post: req.params.id,
       user: req.user._id,
-      body: req.body.body
-    })
+      body: req.body.body,
+    });
 
-    const createdComment = await Comment.findOne({post:req.params.id,user:req.user._id});
+    const createdComment = await Comment.findOne({ post: req.params.id, user: req.user._id });
 
     //게시글에 댓글 추가
-    const post = await Board.findOne({_id:req.params.id});
-    await post.update(
-      {$push: {comment: createdComment._id}}
-    )
+    const post = await Board.findOne({ _id: req.params.id });
+    await post.update({ $push: { comment: createdComment._id } });
 
     return res.json({});
   } catch (e) {
@@ -172,10 +161,10 @@ router.post('/boards/:id/comments', fetchUser, async (req, res) => {
 // (async/await)
 router.post('/comments/:id/update', fetchUser, async (req, res) => {
   try {
-    const comment = await Comment.findOne({_id: req.params.id})
+    const comment = await Comment.findOne({ _id: req.params.id });
     await comment.update({
-      body: req.body.body
-    })
+      body: req.body.body,
+    });
 
     return res.json({});
   } catch (e) {
@@ -191,27 +180,25 @@ router.post('/comments/:id/update', fetchUser, async (req, res) => {
 // (async/await)
 router.post('/comments/:id/recommendation', fetchUser, async (req, res) => {
   try {
-    const comment = await Comment.findOne({_id: req.params.id});
-    const recommend_user = await comment.exists({recommend_user: req.params.id});
+    const comment = await Comment.findOne({ _id: req.params.id });
+    const recommend_user = await comment.exists({ recommend_user: req.params.id });
 
     //작성자이거나 이미 추천한 유저인 경우 에러 출력
-    if(req.params.id==comment.user){
+    if (req.params.id == comment.user) {
       return res.json({
-        error: "INVALID RECOMMENDED USER"
-      })
-    }
-    else if(recommend_user != null){
+        error: 'INVALID RECOMMENDED USER',
+      });
+    } else if (recommend_user != null) {
       await comment.update(
-        {$set: {recommendation: comment.recommendation - 1,}},
-        {$pull: {recommend_user: req.params.id}}
-      )
-      return res.json({})
-    }
-    else{
+        { $set: { recommendation: comment.recommendation - 1 } },
+        { $pull: { recommend_user: req.params.id } }
+      );
+      return res.json({});
+    } else {
       await comment.update(
-        {$set: {recommendation: comment.recommendation + 1,}},
-        {$push: {recommend_user: req.params.id}}
-      )
+        { $set: { recommendation: comment.recommendation + 1 } },
+        { $push: { recommend_user: req.params.id } }
+      );
       return res.json({});
     }
   } catch (e) {
@@ -228,11 +215,11 @@ router.post('/comments/:id/recommendation', fetchUser, async (req, res) => {
 router.post('/comments/:id/delete', fetchUser, async (req, res) => {
   try {
     //게시글에서 댓글 삭제
-    const post = await Board.findOne({comment: req.params.id});
-    await post.update({$pull: {comment: req.params.id}});
+    const post = await Board.findOne({ comment: req.params.id });
+    await post.update({ $pull: { comment: req.params.id } });
 
     //댓글 삭제
-    await Comment.deleteOne({_id: req.params.id})
+    await Comment.deleteOne({ _id: req.params.id });
 
     return res.json({});
   } catch (e) {
