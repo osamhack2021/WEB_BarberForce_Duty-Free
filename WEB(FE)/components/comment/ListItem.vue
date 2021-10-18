315b1325961369f24275e342c06308551073a109
Bisue <bdu00chch@naver.com>
2021-10-19 03:50:14 +0900

    댓글 컴포넌트 디자인 수정

--- WEB(FE)/components/comment/ListItem.vue
+++ WEB(FE)/components/comment/ListItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-white border-b border-gray-300 sm:border-l sm:border-r py-2 px-4">
-    <div class="flex items-center text-xs">
+    <div class="flex items-center text-xs mb-1">
       <span class="flex items-center">
         <img class="mr-1" width="26" height="26" :src="avatar" />
         <span>{{ comment.user.rank }} {{ comment.user.name }}</span>
@@ -9,7 +9,7 @@
         {{ createdAt }}
       </span>
     </div>
-    <div>{{ comment.body }}</div>
+    <div class="text-sm mb-1">{{ comment.body }}</div>
     <div class="flex items-center text-xs">
       <div class="flex items-center text-gray-300 ml-auto">
         <button class="flex item-center" @click="recommend">
