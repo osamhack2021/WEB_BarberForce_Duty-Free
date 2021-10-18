5537b0df4653b6d6a3936f8e04bc189a6162c0c3
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2021-10-19 00:04:20 +0900

    카카오 회원가입 오류 수정 (#83)
    * 게시판 기능 추가중
    
    * add bulletin and userDB schema change
    
    * add profiles/update nickname, rank
    
    * add router boards.js
    
    * err fix
    
    * fix api with notion api document
    
    * add Cut Price in description
    
    * board function test
    
    * fix conflict boards.js
    
    * console.log change, board function test
    
    * bulletin test
    
    * kakao register fix
    
    * /me no social id error
    
    * syntax err fix

--- WEB(BE)/routes/auth.js
+++ WEB(BE)/routes/auth.js
@@ -108,7 +108,7 @@ router.get('/me', fetchUser, async (req, res) => {
   try {
     if(user.soldier_id == null){
       return res.status(403).json({
-        error: NO_SOCIAL_ID
+        error: 'NO_SOCIAL_ID'
       })
     }
     else{
