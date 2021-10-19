53760d2c7d9864fa2750c4a717e9a89d90d3f05e
Bisue <bdu00chch@naver.com>
2021-10-19 18:37:03 +0900

    댓글 수정 폼

--- WEB(FE)/components/comment/ListItem.vue
+++ WEB(FE)/components/comment/ListItem.vue
@@ -5,12 +5,29 @@
         <img class="mr-1" width="26" height="26" :src="avatar" />
         <span>{{ comment.user.rank }} {{ comment.user.name }}</span>
       </span>
-      <span class="text-gray-400 ml-auto">
+      <span class="text-gray-300 ml-auto">
         {{ createdAt }}
       </span>
     </div>
     <div class="text-sm mb-1">{{ comment.body }}</div>
     <div class="flex items-center text-xs">
+      <template v-if="mine">
+        <button class="text-xs rounded bg-brand text-white py-1 px-2 mr-2" @click="openEditModal">수정</button>
+        <button class="text-xs rounded bg-red-500 text-white py-1 px-2" @click="remove">삭제</button>
+        <CommonModal :opened="editModal">
+          <div class="p-2">
+            <textarea
+              class="rounded border border-gray-300 p-2 text-sm"
+              type="text"
+              v-model="this.commentForm.body"
+            ></textarea>
+            <div class="flex justify-end items-center">
+              <button class="text-xs rounded bg-red-500 text-white py-1 px-2 mr-2" @click="closeEditModal">취소</button>
+              <button class="text-xs rounded bg-brand text-white py-1 px-2" @click="editSubmit">수정</button>
+            </div>
+          </div>
+        </CommonModal>
+      </template>
       <div class="flex items-center text-gray-300 ml-auto">
         <button class="flex items-center" @click="recommend">
           <img v-if="recommended" class="mr-1" src="@/assets/img/recommend.svg" />
@@ -32,6 +49,15 @@ export default {
       required: true,
     },
   },
+  data() {
+    console.log(this.comment.body);
+    return {
+      commentForm: {
+        body: this.comment.body,
+      },
+      editModal: false,
+    };
+  },
   computed: {
     avatar() {
       return this.comment.user.avatar || '/img/avatar-placeholder.svg';
@@ -42,6 +68,9 @@ export default {
     recommended() {
       return !!this.comment.recommend_user.find(user => user._id === this.$store.state.auth.user._id);
     },
+    mine() {
+      return this.comment.user._id === this.$store.state.auth.user._id;
+    },
   },
   methods: {
     async recommend() {
@@ -49,6 +78,35 @@ export default {
         await this.$api.comment.recommend(this.comment._id);
         this.$emit('reload');
       } catch (e) {
+        this.$toast.error('에러가 발생했습니다!');
+      }
+    },
+    openEditModal() {
+      this.editModal = true;
+    },
+    closeEditModal() {
+      this.editModal = false;
+      this.commentForm.body = this.comment.body;
+    },
+    async editSubmit() {
+      try {
+        await this.$api.comment.update(this.comment._id, this.commentForm);
+        this.closeEditModal();
+        this.$toast.success('댓글을 수정했습니다!');
+        this.$emit('reload');
+      } catch (e) {
+        console.error(e);
+        this.$toast.error('에러가 발생했습니다!');
+      }
+    },
+    async remove() {
+      try {
+        if (confirm('정말 삭제하시겠습니까?')) {
+          await this.$api.comment.delete(this.comment._id);
+          this.$emit('reload');
+        }
+      } catch (e) {
+        console.error(e);
         this.$toast.error('에러가 발생했습니다!');
       }
     },
