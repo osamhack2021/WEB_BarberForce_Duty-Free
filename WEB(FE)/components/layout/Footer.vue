138399f15a7efe2c6adc2deb607e899e76f4210d
Chanhyuk Byeon <bdu00chch@naver.com>
2021-10-20 00:21:16 +0900

    푸터 (#102)

--- WEB(FE)/components/layout/Footer.vue
+++ WEB(FE)/components/layout/Footer.vue
@@ -1,7 +1,17 @@
 <template>
-  <footer class="bg-gray-500">
+  <footer class="bg-gray-600 text-white">
     <div class="container p-3">
-      <div></div>
+      <div class="flex">
+        <div>
+          <div class="text-xl mb-1">Team</div>
+          <div class="text-sm text-gray-200">병장 이세, 병장 박상욱, 병장 변찬혁</div>
+        </div>
+        <div class="flex justify-items-center items-center ml-auto">
+          <a href="https://github.com/osamhack2021/WEB_BarberForce_Duty-Free" target="_blank">
+            <img width="48" src="@/assets/img/github.svg" />
+          </a>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
